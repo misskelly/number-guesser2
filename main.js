89b73928e8b9bb2a859bd2f8c7e29bb44a097ff9
Kelly Zick <kllyzck@gmail.com>
2019-01-30 14:40:37 -0700

    Current range text updates on dom

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+var minRange = document.querySelector('.min-range');
+var maxRange = document.querySelector('.max-range');
+var updateBtn = document.querySelector('.update');
+var currentMin = document.querySelector('.range-start');
+var currentMax = document.querySelector('.range-end');
 
+updateBtn.addEventListener('click', updateRange);
+
+function updateRange() {
+    currentMin.innerText = minRange.value;
+    currentMax.innerText = maxRange.value;
+}
+
+
+// checkValidity()
